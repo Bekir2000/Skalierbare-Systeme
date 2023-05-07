0009d722e8496543eb7a8da8792fde451f232971
Armin Sprejz <armin@Voyager.local>
2023-05-07 17:54:31 +0200

    added logic

--- server.js
+++ server.js
@@ -1,75 +1,65 @@
-var cookieParser = require("cookie-parser");
-var express = require("express");
-var path = require("path");
+const db = require('./db.js');
+const express = require('express');
 
-var app = express();
-
-
+const app = express();
 const PORT = 8004;
 
-let db = [   
-    {
-        id: 0,
-        description:"Beispielseite mit dem Bootstrap Framework anlegen",
-        deadline:"03/05/2023",
-        percentage:20
+app.use(express.static('./public'))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-    },
-    {
-        id: 1,
-        description:"Irgendwas machen",
-        deadline:"04/03/2022",
-        percentage:50
-
-    }
-]
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-
-app.post("/newtodo.html",(req,res)=>{
-    
-    
-    let task = req.body;
-    console.log('created: ')
-    console.log(task)
-
-    if(task.description && task.deadline && task.percentage){
-
-        if(db.length != 0){
-            task.id = db.slice(-1)[0].id + 1
-        }else{
-            task.id = 0
+app.get('/api/todos', (req, res) => {
+    db.getTodos((err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(rows);
         }
+    });
+});
 
-        db.push(task)
-    }
-    
+app.get('/api/todos/:id', (req, res) => {
+    const { id } = req.params;
+    db.getTodoId(id, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(row);
+        }
+    });
+});
+
+app.post('/api/newtodo', (req, res) => {
+    const { description, deadline, percentage } = req.body
+    db.addTodo(description, deadline, percentage, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(row);
+        }
+    });
 })
 
-app.get("/getTasks",(req,res)=>{
-    
-    
-    res.status(200).json(db)
+app.put('/api/edittodo/:id', (req, res) => {
+    const { id } = req.params;
+    const { description, deadline, percentage } = req.body
+    db.editTodo(description, deadline, percentage, id, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(row)
+        }
+    });
 })
 
-app.delete("/deleteButton", (req, res)=>{
-    
-
-    let task = req.body
-    db = db.filter(item => item.id != task.id)
-
-    console.log("deleted: ")
-    console.log(task)
-    
-    res.sendStatus(200)
+app.delete('/api/deletetodo/:id', (req, res) => {
+    const { id } = req.params;
+    db.deleteTodo(id, (err, row) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        } else {
+            res.json(row);
+        }
+    });
 })
-
-
-
-
-app.use("/",express.static("public"))
-
-
 app.listen(PORT, ()=> console.log(`listen on http://localhost:${PORT}`))
